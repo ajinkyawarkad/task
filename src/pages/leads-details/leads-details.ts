54143cedd5704ffd29b669ff90f8d49f3ac1e03b
Vishal <vishal.passogen@gmail.com>
2021-07-07 17:10:23 +0530

    sdfghjkl

--- src/pages/leads-details/leads-details.ts
+++ src/pages/leads-details/leads-details.ts
@@ -492,12 +492,12 @@ export class LeadsDetailsPage {
   }
 
   ionViewDidLoad() {
-
+    
 
     function closeForm() {
       $(".form-popup-bg").removeClass("is-visible");
     }
-
+    
     $(document).ready(function ($) {
       /* Contact Form Interactions */
       $("#btnOpenForm").on("click", function (event) {
@@ -517,6 +517,28 @@ export class LeadsDetailsPage {
         }
       });
     });
+
+
+     $(document).ready(function ($) {
+      /* Contact Form Interactions */
+      $("#btnOpenForm").on("click", function (event) {
+        event.preventDefault();
+
+        $(".form-popup-bg").addClass("is-visible");
+      });
+
+      //close popup when clicking x or off popup
+      $(".form-popup-bg").on("click", function (event) {
+        if (
+          $(event.target).is(".form-popup-bg") ||
+          $(event.target).is("#btnCloseForm")
+        ) {
+          event.preventDefault();
+          $(this).removeClass("is-visible");
+        }
+      });
+    });
+
 
     $("#fixed-form-container .body").hide();
     $("#fixed-form-container .button").click(function () {
