f1374a47b94a9d5b53a85e4c22af3657d50d2d6b
Vishal <vishal.passogen@gmail.com>
2021-07-14 19:48:05 +0530

    sdfghjk

--- src/pages/task-details/task-details.ts
+++ src/pages/task-details/task-details.ts
@@ -1,29 +1,13 @@
 import { Component, ViewChild } from "@angular/core";
-import {
-  AlertController,
-  IonicPage,
-  NavController,
-  NavParams,
-} from "ionic-angular";
+import {AlertController,NavController,NavParams,} from "ionic-angular";
 import { Leadd, Leadref } from "../../models/user";
-
 import firebase, { database, firestore } from "firebase/app";
 import { Storage } from "@ionic/storage";
-
 import { AngularFirestore } from "@angular/fire/firestore";
 import { AngularFireAuth } from "@angular/fire/auth";
-
-import { uuid } from "uuidv4";
+import { v4 as uuid } from "uuid";
 import { Observable } from "rxjs";
-import * as $ from "jquery";
-import { Slides } from "ionic-angular";
-import { LeadsDetailsPage } from "../leads-details/leads-details";
-import { TrackCampaignPage } from "../track-campaign/track-campaign";
 
-interface Lead {
-  status: string;
-  action: string;
-}
 interface Task {
   status: string;
   action: string;
@@ -36,8 +20,7 @@ interface Task {
   templateUrl: "task-details.html",
 })
 export class TaskDetailsPage {
-  @ViewChild(Slides) slides: Slides;
-  slideOpts;
+
   public hideMe: boolean = false;
   public hideMe1: boolean = false;
   myDate;
@@ -55,7 +38,8 @@ export class TaskDetailsPage {
   leadd = {} as Leadd;
   showSlide = false;
   count;
-  public products: Observable<Lead[]>;
+  public products: Observable<any[]>;
+  currentuser = firebase.auth().currentUser;
 
   constructor(
     public navCtrl: NavController,
@@ -65,98 +49,53 @@ export class TaskDetailsPage {
     private auth: AngularFireAuth,
     public alertCtrl: AlertController
   ) {
-    // this.value = navParams.get('cid');
-    // console.log(this.value);
+    
 
     this.cid = navParams.get("cid");
-    console.log(this.cid);
-
     this.data = navParams.get("data");
-    console.log("Data", this.data);
-
-    this.slideOpts = {
-      effect: "flip",
-    };
-
-    let currentuser = firebase.auth().currentUser;
+   
     firebase
       .firestore()
       .collection("Company")
-      .doc(currentuser.photoURL + "/" + "Campaigns" + "/" + this.cid)
+      .doc(this.currentuser.photoURL + "/" + "Campaigns" + "/" + this.cid)
       .onSnapshot((doc) => {
         var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-        console.log(source, " data: ");
         this.products = doc.data().status;
         this.arr = this.products;
-        console.log(this.products);
       });
   }
 
   ionViewDidLoad() {
-    let currentuser = firebase.auth().currentUser;
-    firebase.firestore().collection("Company").doc(currentuser.photoURL).collection("Campaigns").doc(this.cid).get().then(doc =>{
+  
+    firebase.firestore().collection("Company").doc(this.currentuser.photoURL).collection("Campaigns").doc(this.cid).get().then(doc =>{
       this.count = doc.data().pendings
       
     })
-    if (this.data.action == null) {
-      console.log("action=Null");
-      this.showSlide = false;
-    } else {
-      console.log("action= ", this.data.action);
-      this.showSlide = true;
-    }
-    this.slides.onlyExternal = true;
+    
+    firebase
+    .firestore()
+    .collection("Company")
+    .doc(this.currentuser.photoURL)
+    .collection("Campaigns")
+    .doc(this.cid)
+    .collection("leads")
+    .doc(this.data.uid)
+    .collection("History")
+    .doc("Activity1").get().then(doc =>{
+      if(doc.exists){
+        console.log("docExists", doc.data().data);      
+      }else{
+        doc.ref.set({
+          taskIds:[]
+        })
+      }
+    })
   }
-  slideToSlide() {
-    if (this.slides.getActiveIndex() + 1 === this.slides.length()) {
-      this.slides.slideTo(0);
-    } else {
-      this.slides.lockSwipeToNext(false);
-      this.slides.slideTo(this.slides.getActiveIndex() + 1);
-      this.slides.lockSwipeToNext(false);
-    }
-  }
-
-  slideToPrev() {
-    if (this.slides.getActiveIndex() + 1 == this.slides.length()) {
-      this.slides.lockSwipeToPrev(false);
-      this.slides.slideTo(this.slides.getActiveIndex() - 1);
-      this.slides.lockSwipeToPrev(false);
-    }
-  }
+ 
 
   completeTask(val) {
-    console.log("Val", val);
-    let currentuser = firebase.auth().currentUser;
-    firebase
-      .firestore()
-      .collection("Company")
-      .doc(
-        currentuser.photoURL+
-          "/" +
-          "Campaigns" +
-          "/" +
-          this.cid +
-          "/" +
-          "leads" +
-          "/" +
-          val.uid
-      )
-      .collection("History")
-      .doc("Activity1")
-      .update({
-        data: firestore.FieldValue.arrayUnion({
-          Time: new Date(),
-          Action: this.data.action,
-          FollowUp: this.data.datetime,
-          Remark: this.data.remark,
-          name: val.uid,
-          Handler: this.data.SR_name,
-          Completed: true,
-        }),
-      });
-
-    // firebase.firestore().collection('Company').doc(currentuser.photoURL).collection('Campaigns').doc(this.cid).collection('leads')
+    // let this.currentuser = firebase.auth().this.currentuser;
+    // firebase.firestore().collection('Company').doc(this.currentuser.photoURL).collection('Campaigns').doc(this.cid).collection('leads')
     //   .doc(this.data.uid).collection('History')
     //   .doc('Activity1')
     //   .set({
@@ -170,7 +109,6 @@ export class TaskDetailsPage {
   Getselected(selected_value) { 
     let temp = [];
 
-    console.log("SELECT", selected_value);
     this.select = selected_value;
     let action;
     for (var s in this.arr) {
@@ -180,14 +118,14 @@ export class TaskDetailsPage {
       }
     }
     this.act = action;
-    console.log("TEMO", this.act);
+
     if (this.act === "Remove client from profile") {
-      let currentuser = firebase.auth().currentUser
+     
       firebase
       .firestore()
       .collection("Company")
       .doc(
-        currentuser.photoURL +
+        this.currentuser.photoURL +
           "/" +
           "Campaigns" +
           "/" +
@@ -215,26 +153,100 @@ export class TaskDetailsPage {
         alert.present();
        
       });
-    console.log("DELETED", this.data.uid);
-    
-      
+  
     }
   }
 
+
+  Reference()
+  {
+    let uid = uuid();
+      
+    let alert = this.alertCtrl.create({
+      title: "Add Reference",
+      inputs: [{ name: "first_name", placeholder: "First Name" },
+               { name: "last_name", placeholder: "Last Name" },
+               { name: "email", placeholder: "Email" },
+               { name: "phone", placeholder: "Phone" }],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          
+        },
+        {
+          text: "Save",
+          handler: (reff) => {
+            if (reff.first_name) {
+              
+           
+              const result =  firebase
+          .firestore()
+          .collection("Company")
+          .doc(
+            this.currentuser.photoURL +
+              "/" +
+              "Campaigns" +
+              "/" +
+              this.cid +
+              "/" +
+              "Lead references" +
+              "/" +
+              uid
+          )
+          .set(
+            Object.assign({
+              id: uid,
+              first_name: reff.first_name,
+              last_name: reff.last_name,
+              email: reff.email,
+              phone: reff.phone,
+              refId: this.data.uid,
+            })
+          );
+
+              if (result) {
+                let alert = this.alertCtrl.create({
+                  title: "Success",
+                  subTitle: "Reference Added",
+                  buttons: [
+                    {
+                      text: "OK",
+                      handler: (data) => {
+                        //this.navCtrl.setRoot(HomePage);
+                      },
+                    },
+                  ],
+                });
+                alert.present();
+              }
+            } else {
+              return false;
+            }
+          },
+        },
+      ],
+    });
+    alert.present();
+  }
+  
+  
+
   Task(task: Task) {
-    console.log("SR name", this.data.SR_name);
+   
+    let taskI = uuid()
+    let idArr = taskI.split("-")
+    let taskId = idArr[0]
+   
     if (task.action && task.remark != null) {
       this.storage.get("cuid").then((val) => {
-        console.log("id is", val);
         let uid = uuid();
-        console.log(uid);
-        let currentuser = firebase.auth().currentUser;
-
+      
         firebase
           .firestore()
           .collection("Company")
           .doc(
-            currentuser.photoURL +
+            this.currentuser.photoURL +
               "/" +
               "Campaigns" +
               "/" +
@@ -255,24 +267,38 @@ export class TaskDetailsPage {
               }
             )
           );
-        console.log("ACT IS ", this.act);
-        console.log("SELECT", this.select);
-
-
-
-        firebase.firestore().collection("Company").doc(currentuser.photoURL).collection("Campaigns").doc(this.cid).collection("leads").doc(this.data.uid).get().then(doc => {
+          firebase
+          .firestore()
+          .collection("Company")
+          .doc(
+            this.currentuser.photoURL +
+              "/" +
+              "Campaigns" +
+              "/" +
+              this.cid +
+              "/" +
+              "leads" +
+              "/" +
+              this.data.uid
+          )
+          .set(
+            
+              {
+                taskId:taskId
+              },{merge:true}
+            
+          );
+    
+        firebase.firestore().collection("Company").doc(this.currentuser.photoURL).collection("Campaigns").doc(this.cid).collection("leads").doc(this.data.uid).get().then(doc => {
           if(doc.data().pending = true){
             doc.ref.update({
               pending:false
             })
 
-            firebase.firestore().collection("Company").doc(currentuser.photoURL).collection("Campaigns").doc(this.cid).update({
+            firebase.firestore().collection("Company").doc(this.currentuser.photoURL).collection("Campaigns").doc(this.cid).update({
               pendings:this.count-1
             })
 
-
-          }else{
-           console.log("not false")
 
           }
         })
@@ -283,7 +309,7 @@ export class TaskDetailsPage {
               .firestore()
               .collection("Company")
               .doc(
-                currentuser.photoURL +
+                this.currentuser.photoURL +
                   "/" +
                   "Campaigns" +
                   "/" +
@@ -302,7 +328,7 @@ export class TaskDetailsPage {
                     " " +
                     "by" +
                     " " +
-                    currentuser.displayName,
+                    this.currentuser.displayName,
                 })
               );
             break;
@@ -311,7 +337,7 @@ export class TaskDetailsPage {
               .firestore()
               .collection("Company")
               .doc(
-                currentuser.photoURL +
+                this.currentuser.photoURL +
                   "/" +
                   "Campaigns" +
                   "/" +
@@ -322,14 +348,14 @@ export class TaskDetailsPage {
                   this.data.uid
               )
               .delete();
-            console.log("DELETED", this.data.uid);
+           
             break;
         }
 
         firebase
           .firestore()
           .collection("Company")
-          .doc(currentuser.photoURL)
+          .doc(this.currentuser.photoURL)
           .collection("Campaigns")
           .doc(this.cid)
           .collection("leads")
@@ -338,7 +364,7 @@ export class TaskDetailsPage {
           .doc("Activity1")
           .set(
             {
-              data: firebase.firestore.FieldValue.arrayUnion({
+              [taskId]: {
                 Time: new Date(),
                 Action: task.action,
                 FollowUp: task.datetime,
@@ -346,10 +372,16 @@ export class TaskDetailsPage {
                 name: this.data.uid,
                 Handler: this.data.SR_name,
                 Completed: false,
-              }),
+                taskId:taskId
+              },
+              taskIds:firestore.FieldValue.arrayUnion(
+                taskId
+              )
             },
             { merge: true }
           );
+
+
         var b = new Date().getMonth() + 1;
 
         var c = new Date().getFullYear();
@@ -358,14 +390,14 @@ export class TaskDetailsPage {
         let date = a + "-" + b + "-" + c;
         let dat = "";
         dat = date;
-        console.log("Dateee", date);
+        
 
         firebase
           .firestore()
           .collection("Company")
-          .doc(currentuser.photoURL)
+          .doc(this.currentuser.photoURL)
           .collection("Admin")
-          .doc(currentuser.uid)
+          .doc(this.currentuser.uid)
           .collection("Report")
           .doc(dat)
           .set(
@@ -414,87 +446,24 @@ export class TaskDetailsPage {
     }
   }
 
-  Save(leadref: Leadref) {
-    if (
-      leadref.email &&
-      leadref.first_name &&
-      leadref.last_name &&
-      leadref.phone != null
-    ) {
-      this.storage.get("cuid").then((val) => {
-        console.log("id is", val);
-        let uid = uuid();
-        console.log(uid);
-        let currentuser = firebase.auth().currentUser;
-
-        firebase
-          .firestore()
-          .collection("Company")
-          .doc(
-            currentuser.photoURL +
-              "/" +
-              "Campaigns" +
-              "/" +
-              this.cid +
-              "/" +
-              "Lead references" +
-              "/" +
-              uid
-          )
-          .set(
-            Object.assign({
-              id: uid,
-              first_name: leadref.first_name,
-              last_name: leadref.last_name,
-              email: leadref.email,
-              phone: leadref.phone,
-              refId: this.data.uid,
-            })
-          );
-
-        let alert = this.alertCtrl.create({
-          title: "Success",
-          subTitle: "Saved Successfully",
-          //scope: id,
-          buttons: [
-            {
-              text: "OK",
-              handler: (data) => {
-                //this.navCtrl.push(UserDetailsPage);
-              },
-            },
-          ],
-        });
-        alert.present();
-      });
-    } else {
-      let alert = this.alertCtrl.create({
-        title: "Warning",
-        subTitle: "Please Insert Data",
-        //scope: id,
-        buttons: [
-          {
-            text: "OK",
-            handler: (data) => {
-              //this.navCtrl.push(LoginPage);
-            },
-          },
-        ],
-      });
-      alert.present();
-    }
-  }
 
   hide(action) {
-    let currentuser = firebase.auth().currentUser;
-    
+   
+    var b = new Date().getMonth() + 1;
+
+    var c = new Date().getFullYear();
+    var a = new Date().getDate();
+
+    let date = a + "-" + b + "-" + c;
+    let dat = "";
+    dat = date;
    
     if(action == "None"){
       firebase
       .firestore()
       .collection("Company")
       .doc(
-        currentuser.photoURL +
+        this.currentuser.photoURL +
           "/" +
           "Campaigns" +
           "/" +
@@ -520,7 +489,7 @@ export class TaskDetailsPage {
       firebase
           .firestore()
           .collection("Company")
-          .doc(currentuser.photoURL)
+          .doc(this.currentuser.photoURL)
           .collection("Campaigns")
           .doc(this.cid)
           .collection("leads")
@@ -537,10 +506,31 @@ export class TaskDetailsPage {
                 name: this.data.uid,
                 Handler: this.data.SR_name,
                 Completed: true,
+                pending:false
               }),
             },
             { merge: true }
 
+          );
+          firebase
+          .firestore()
+          .collection("Company")
+          .doc(this.currentuser.photoURL)
+          .collection("Admin")
+          .doc(this.currentuser.uid)
+          .collection("Report")
+          .doc(dat)
+          .set(
+            {
+              data: firebase.firestore.FieldValue.arrayUnion({
+                Time: new Date(),
+                Action: "None",
+                FollowUp: "",
+                Remark: this.task.remark,
+                name: this.data.uid,
+              }),
+            },
+            { merge: true }
           );
 
           let alert = this.alertCtrl.create({
@@ -558,7 +548,7 @@ export class TaskDetailsPage {
           });
           alert.present();
       
-    }else{
+    }else{                //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
       this.hideMe = true;
       
     }
@@ -571,11 +561,8 @@ export class TaskDetailsPage {
     this.navCtrl.pop();
   }
   change(datePicker) {
-    console.log("date", this.myDate);
-    console.log("datePicker", datePicker);
+   
     datePicker.open();
   }
-  ab() {
-    console.log("date");
-  }
+  
 }
